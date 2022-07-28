eecf9901600bab42518f2bdaaa818db7033c93e0
notsoyoungg <p.sergey1997@mail.ru>
2022-07-28 15:49:05 +0600

    fix 'gendiff.py'

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,9 +1,11 @@
 from gendiff.formatters.stylish import stylish
 from gendiff.formatters.plain import plain
 from gendiff.formatters.json import jsonn
+from gendiff.make_diff import make_diff
 
 
-def generate_diff(diff, format='stylish'):
+def generate_diff(file1, file2, format='stylish'):
+    diff = make_diff(file1, file2)
     if format == 'plain':
         return plain(diff)
     if format == 'json':
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,7 @@ from gendiff.gendiff import generate_diff
 
 
 def main():
-    print(generate_diff(make_diff(pars.readed, pars.readed2), pars.args.format))
+    print(generate_diff(pars.readed, pars.readed2, pars.args.format))
 
 
 if __name__ == '__main__':
